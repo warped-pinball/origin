bc92f6dc5997094272626fff4b2bf704e5071141
Maxwell Mullin <mullinmax@gmail.com>
2025-07-30 22:36:08 -0400

    fix e2e test skip

--- app/crud.py
+++ app/crud.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 from passlib.context import CryptContext
 from typing import List, Optional
+import secrets
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -19,10 +20,13 @@ def get_user(db: Session, user_id: int) -> Optional[models.User]:
 
 def create_user(db: Session, user: schemas.UserCreate) -> models.User:
     hashed_password = pwd_context.hash(user.password)
+    verification_token = secrets.token_urlsafe(32)
     db_user = models.User(
         email=user.email,
         hashed_password=hashed_password,
         screen_name=user.screen_name,
+        verification_token=verification_token,
+        is_verified=False,
     )
     db.add(db_user)
     db.commit()
@@ -47,6 +51,38 @@ def update_user(db: Session, user: models.User, updates: schemas.UserUpdate) -> 
 
 def update_user_password(db: Session, user: models.User, password: str) -> models.User:
     user.hashed_password = pwd_context.hash(password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+
+def verify_user(db: Session, token: str) -> Optional[models.User]:
+    user = db.query(models.User).filter(models.User.verification_token == token).first()
+    if not user:
+        return None
+    user.is_verified = True
+    user.verification_token = None
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+
+def create_reset_token(db: Session, user: models.User) -> str:
+    token = secrets.token_urlsafe(32)
+    user.reset_token = token
+    db.add(user)
+    db.commit()
+    return token
+
+
+def reset_password(db: Session, token: str, password: str) -> Optional[models.User]:
+    user = db.query(models.User).filter(models.User.reset_token == token).first()
+    if not user:
+        return None
+    user.hashed_password = pwd_context.hash(password)
+    user.reset_token = None
     db.add(user)
     db.commit()
     db.refresh(user)
