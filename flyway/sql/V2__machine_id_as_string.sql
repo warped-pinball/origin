327ffd7e50659763654cbc2acea6418e7fea4291
Maxwell Mullin <mullinmax@gmail.com>
2025-09-07 13:27:19 -0400

    fix migration ordering to drop dependent tables

--- flyway/sql/V2__machine_id_as_string.sql
+++ flyway/sql/V2__machine_id_as_string.sql
@@ -1,7 +1,14 @@
--- Change machine-related IDs to VARCHAR to support UUIDs
+--
+-- Existing tables are renamed first so that dependent foreign keys continue
+-- to reference the old versions until new tables are populated.
+--
+
+-- Rename tables
+ALTER TABLE machines RENAME TO machines_old;
+ALTER TABLE scores RENAME TO scores_old;
+ALTER TABLE qr_codes RENAME TO qr_codes_old;
 
 -- Machines table
-ALTER TABLE machines RENAME TO machines_old;
 CREATE TABLE machines (
     id VARCHAR PRIMARY KEY,
     game_title VARCHAR NOT NULL,
@@ -11,10 +18,8 @@ CREATE TABLE machines (
 );
 INSERT INTO machines (id, game_title, shared_secret, user_id, location_id)
     SELECT CAST(id AS VARCHAR), game_title, shared_secret, user_id, location_id FROM machines_old;
-DROP TABLE machines_old;
 
 -- Scores table
-ALTER TABLE scores RENAME TO scores_old;
 CREATE TABLE scores (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id),
@@ -25,10 +30,8 @@ CREATE TABLE scores (
 );
 INSERT INTO scores (id, user_id, machine_id, game, value, created_at)
     SELECT id, user_id, CAST(machine_id AS VARCHAR), game, value, created_at FROM scores_old;
-DROP TABLE scores_old;
 
 -- QR codes table
-ALTER TABLE qr_codes RENAME TO qr_codes_old;
 CREATE TABLE qr_codes (
     id SERIAL PRIMARY KEY,
     url VARCHAR UNIQUE NOT NULL,
@@ -40,4 +43,8 @@ CREATE TABLE qr_codes (
 );
 INSERT INTO qr_codes (id, url, created_at, generated_at, nfc_link, user_id, machine_id)
     SELECT id, url, created_at, generated_at, nfc_link, user_id, CAST(machine_id AS VARCHAR) FROM qr_codes_old;
+
+-- Drop old tables once data has been migrated
 DROP TABLE qr_codes_old;
+DROP TABLE scores_old;
+DROP TABLE machines_old;
